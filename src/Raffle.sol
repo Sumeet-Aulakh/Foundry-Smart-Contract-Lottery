2fb851b52a9e60e316843701de7e65cd824ae9af
Sumeet Singh Aulakh <sumeetbinjal@gmail.com>
2023-12-28 17:56:43 -0600

    Delete Counter script, Counter contract, and Counter test

---
+++ src/Raffle.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title A Sample Raffle Contract
+ * @author Sumeet Singh Aulakh
+ * @notice This Contract is for creating a sample raffle.
+ * @dev Implements Chainlink VRFv2
+ */
+contract Raffle {
+
+}
