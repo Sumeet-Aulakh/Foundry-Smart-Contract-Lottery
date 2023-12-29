3710d9512f439537a5eea0fa2e5a1433388045aa
Sumeet Singh Aulakh <sumeetbinjal@gmail.com>
2023-12-28 19:17:52 -0600

    Add Raffle contract layout and entrance fee functionality

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,24 @@
+// Layout of Contract
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// type declarations
+// state variables
+// events
+// modifiers
+// function
+
+// Layout of Function
+// contructor
+//  receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view and pure functions
+
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
@@ -9,5 +30,48 @@ pragma solidity ^0.8.18;
  * @dev Implements Chainlink VRFv2
  */
 contract Raffle {
+    error Raffle__NotEnoughEthSend();
 
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    /** Events */
+    event EnteredRaffle(address indexed player);
+
+    /** Functions */
+
+    constructor(uint256 entranceFee, uint256 interval) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+    }
+
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent");
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthSend();
+        }
+        s_players.push(payable(msg.sender));
+        // 1. Makes Migration easier
+        // 2. Make front-end "indexing" easier
+        emit EnteredRaffle(msg.sender);
+    }
+
+    // 1. Get a random number
+    // 2. Use random number to pick a winner
+    // 3. Be automatically called
+    function pickWinner() public {
+        // Check to see if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+    }
+
+    /** Getter Functions */
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
 }
