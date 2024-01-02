3d6691a4a7f99889e4b4a198643a625c3d0e6f86
Sumeet Singh Aulakh <sumeetbinjal@gmail.com>
2024-01-02 11:37:33 -0600

    Add CreateSubscription, FundSubscription, and AddConsumer interactions

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
-import {CreateSubscription} from "./Interactions.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() external returns (Raffle, HelperConfig) {
@@ -27,6 +27,13 @@ contract DeployRaffle is Script {
                 vrfCoordinator
             );
             // Now fund the subscription
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                vrfCoordinator,
+                subscriptionId,
+                link
+            );
+            AddConsumer addConsumer = new AddConsumer();
         }
 
         // Before Start Broadcast not a real tx
